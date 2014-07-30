be994c84fe1a0601931be8e7dc7300751f458da5
Maxiwell <maxiwell@cajarana>
2014-07-30 16:56:37 -0300

    update daemon_nightly.sh

--- daemon_nightly.sh
+++ daemon_nightly.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-#./nightlytester.sh
-rsync -Rrazp -v public_html cajarana:/home/lsc/projetos/archc/acnightlytester/
+export LD_LIBRARY_PATH="/local/archc/acnightlytester/tools/systemc-2.3.0/lib-linux64/"
+cd /local/archc/acnightlytester &> /dev/null
+./nightlytester.sh
+rsync -Rrazp -v public_html /home/lsc/projetos/archc/acnightlytester/
+cd - &> /dev/null
 

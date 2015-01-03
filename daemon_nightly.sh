0353610f55886795a5aed3c45efab455f6e082da
Maxiwell Node17 <maxiwell@node17>
2015-01-03 03:40:53 -0200

    Move the 'cd' command in first line

--- daemon_nightly.sh
+++ daemon_nightly.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+cd /local/archc/acnightlytester &> /dev/null
 . site.conf
 export LD_LIBRARY_PATH="$SYSTEMCPATH/lib-linux64/"
-cd /local/archc/acnightlytester &> /dev/null
 ./nightlytester.sh site.conf  $1
 rsync -Rrazp -v public_html /home/lsc/projetos/archc/acnightlytester/
 cd - &> /dev/null

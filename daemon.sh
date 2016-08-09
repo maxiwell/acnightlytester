eeadbb10cb5cf585ef8ae0d9cfffac011944941a
Maxiwell Node17 <maxiwell@node17>
2016-08-09 19:45:49 -0300

    Removing shell scripts and unecessary files

--- daemon.sh
+++ daemon.sh
@@ -3,11 +3,11 @@
 ############################################################################
 #
 # This script will be called by cron:
-# 0 0 * * * /home/lsc/projetos/archc/nightly/acnightlytester/daemon_nightly.sh 
+# 0 0 * * * /home/lsc/projetos/archc/nightly/acnightlytester/daemon.sh 
 #
 ###########################################################################
 
 cd /home/lsc/projetos/archc/nightly/acnightlytester/
-./nightlytester.sh conf/site.conf --condor $1
+./acnightly.py conf/config.yaml $1 --condor
 #rsync -Rrazp -v public_html /home/lsc/projetos/archc/acnightlytester/
 
